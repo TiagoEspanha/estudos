08c7437685c7dfcb69024c59c57d4485f3e97a19
Tiago Espanha <tiago.espanha@gmail.com>
2021-10-30 17:32:32 -0300

    ingles

---
+++ faculdade/2020-2/BDNC/atividade2/dimensao_moeda_202109171719.sql
@@ -0,0 +1,84 @@
+INSERT INTO public.dimensao_moeda (moeda) VALUES
+	 ('Afghani'),
+	 ('Ariary'),
+	 ('Baht'),
+	 ('Balboa'),
+	 ('Birr'),
+	 ('Bolivar'),
+	 ('Boliviano'),
+	 ('Cedi'),
+	 ('Colon'),
+	 ('Cordoba');
+INSERT INTO public.dimensao_moeda (moeda) VALUES
+	 ('Dalasi'),
+	 ('Denar'),
+	 ('Dinar'),
+	 ('Dirham'),
+	 ('Dobra'),
+	 ('Dollar'),
+	 ('Dong'),
+	 ('Dram'),
+	 ('Euro'),
+	 ('Forint');
+INSERT INTO public.dimensao_moeda (moeda) VALUES
+	 ('Franc'),
+	 ('Gourde'),
+	 ('Guarani'),
+	 ('Hryvnia'),
+	 ('Kina'),
+	 ('Kip'),
+	 ('Koruna'),
+	 ('Krona'),
+	 ('Krone'),
+	 ('Kuna');
+INSERT INTO public.dimensao_moeda (moeda) VALUES
+	 ('Kwacha'),
+	 ('Kwanza'),
+	 ('Kyat'),
+	 ('Lari'),
+	 ('Lek'),
+	 ('Lempira'),
+	 ('Leone'),
+	 ('Leu'),
+	 ('Lev'),
+	 ('Lilangeni');
+INSERT INTO public.dimensao_moeda (moeda) VALUES
+	 ('Lira'),
+	 ('Litas'),
+	 ('Manat'),
+	 ('Marka'),
+	 ('Metical'),
+	 ('Naira'),
+	 ('Nakfa'),
+	 ('Ngultrum'),
+	 ('Ouguiya'),
+	 ('Pa''anga');
+INSERT INTO public.dimensao_moeda (moeda) VALUES
+	 ('Peso'),
+	 ('Pound'),
+	 ('Pula'),
+	 ('Quetzal'),
+	 ('Rand'),
+	 ('Real'),
+	 ('Rial'),
+	 ('Riels'),
+	 ('Ringgit'),
+	 ('Ruble');
+INSERT INTO public.dimensao_moeda (moeda) VALUES
+	 ('Rupee'),
+	 ('Rupiah'),
+	 ('Shekel'),
+	 ('Shilling'),
+	 ('Sol'),
+	 ('Som'),
+	 ('Somoni'),
+	 ('Taka'),
+	 ('Tala'),
+	 ('Tenge');
+INSERT INTO public.dimensao_moeda (moeda) VALUES
+	 ('Tugrik'),
+	 ('Vatu'),
+	 ('Won'),
+	 ('Yen'),
+	 ('Yuan Renminbi'),
+	 ('Zloty');
---
+++ faculdade/2020-2/BDNC/bruno/atividade2/d_moeda.sql
@@ -0,0 +1,84 @@
+INSERT INTO public.d_moeda (moeda,id) VALUES
+	 ('Afghani','0'),
+	 ('Ariary','1'),
+	 ('Baht','2'),
+	 ('Balboa','3'),
+	 ('Birr','4'),
+	 ('Bolivar','5'),
+	 ('Boliviano','6'),
+	 ('Cedi','7'),
+	 ('Colon','8'),
+	 ('Cordoba','9');
+INSERT INTO public.d_moeda (moeda,id) VALUES
+	 ('Dalasi','10'),
+	 ('Denar','11'),
+	 ('Dinar','12'),
+	 ('Dirham','13'),
+	 ('Dobra','14'),
+	 ('Dollar','15'),
+	 ('Dong','16'),
+	 ('Dram','17'),
+	 ('Euro','18'),
+	 ('Forint','19');
+INSERT INTO public.d_moeda (moeda,id) VALUES
+	 ('Franc','20'),
+	 ('Gourde','21'),
+	 ('Guarani','22'),
+	 ('Hryvnia','23'),
+	 ('Kina','24'),
+	 ('Kip','25'),
+	 ('Koruna','26'),
+	 ('Krona','27'),
+	 ('Krone','28'),
+	 ('Kuna','29');
+INSERT INTO public.d_moeda (moeda,id) VALUES
+	 ('Kwacha','30'),
+	 ('Kwanza','31'),
+	 ('Kyat','32'),
+	 ('Lari','33'),
+	 ('Lek','34'),
+	 ('Lempira','35'),
+	 ('Leone','36'),
+	 ('Leu','37'),
+	 ('Lev','38'),
+	 ('Lilangeni','39');
+INSERT INTO public.d_moeda (moeda,id) VALUES
+	 ('Lira','40'),
+	 ('Litas','41'),
+	 ('Manat','42'),
+	 ('Marka','43'),
+	 ('Metical','44'),
+	 ('Naira','45'),
+	 ('Nakfa','46'),
+	 ('Ngultrum','47'),
+	 ('Ouguiya','48'),
+	 ('Pa''anga','49');
+INSERT INTO public.d_moeda (moeda,id) VALUES
+	 ('Peso','50'),
+	 ('Pound','51'),
+	 ('Pula','52'),
+	 ('Quetzal','53'),
+	 ('Rand','54'),
+	 ('Real','55'),
+	 ('Rial','56'),
+	 ('Riels','57'),
+	 ('Ringgit','58'),
+	 ('Ruble','59');
+INSERT INTO public.d_moeda (moeda,id) VALUES
+	 ('Rupee','60'),
+	 ('Rupiah','61'),
+	 ('Shekel','62'),
+	 ('Shilling','63'),
+	 ('Sol','64'),
+	 ('Som','65'),
+	 ('Somoni','66'),
+	 ('Taka','67'),
+	 ('Tala','68'),
+	 ('Tenge','69');
+INSERT INTO public.d_moeda (moeda,id) VALUES
+	 ('Tugrik','70'),
+	 ('Vatu','71'),
+	 ('Won','72'),
+	 ('Yen','73'),
+	 ('Yuan Renminbi','74'),
+	 ('Zloty','75');
